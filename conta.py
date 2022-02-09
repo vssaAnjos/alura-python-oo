2bd7caa02e63d9e539b4152fa177e70e676dcf99
vssaAnjos <anjnessa9@gmail.com>
2022-02-09 12:13:08 -0300

    Criando construtor

--- conta.py
+++ conta.py
@@ -1,3 +1,9 @@
-
 class Conta:
-    pass
+    # CONSTRUTOR
+    def __init__(self, numero, titular, limite):
+        print("Construindo o objeto...{}".format(self))
+        self.numero = numero
+        self.titular = titular
+        self.limite = limite
+
+        print("Objeto - {}".format(self.titular))
