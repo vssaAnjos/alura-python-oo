396c9c359d7a1252a1f1e553b2da80316f422db5
vssaAnjos <anjnessa9anjos@gmail.co>
2022-02-22 20:28:03 -0300

    criando métodos

--- conta.py
+++ conta.py
@@ -1,9 +1,20 @@
 class Conta:
     # CONSTRUTOR
-    def __init__(self, numero, titular, limite):
+    def __init__(self, numero, titular, limite, saldo):
         print("Construindo o objeto...{}".format(self))
         self.numero = numero
         self.titular = titular
         self.limite = limite
+        self.saldo = saldo
 
         print("Objeto - {}".format(self.titular))
+
+    def extrato(self):
+        print("Saldo de {} do Titular {}".format(self.saldo, self.titular))
+
+    def depositar(self, valor):
+        self.saldo += valor
+
+    def sacar(self, valor):
+        self.saldo -= valor
+
